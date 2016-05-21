da2df74f49d0c4db2bd427cff37c9dfd26cdecc1
Bartezz <bhelak1991@gmail.com>
2016-05-21 19:58:50 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -24,10 +24,11 @@ function slider(a,b,c,d) {
 	var left = "#slider-item-" + d;
 	var right = "#slider-item-" + b;
 	var under = "#slider-item-" + c;
-	$('.slider-item').delay(400).queue(function(){$(this).removeClass("z-1");}).dequeue();
 	console.log(main);
 	$(main).animate({left:0},400).addClass("z-1");
 	$(right).animate({left:940},400);
 	$(under).animate({left:0},400);
 	$(left).animate({left:-940},400);
+	$('.slider-item').delay(400).queue(function(){$(this).removeClass("z-1");}).dequeue();
+
 }
