163378be6cec928cb27755dc6727657d71818d4f
Bartezz <bhelak1991@gmail.com>
2016-05-21 20:15:57 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -25,11 +25,12 @@ function slider(a,b,c,d) {
 	var right = "#slider-item-" + b;
 	var under = "#slider-item-" + c;
 	
-	var checker = $('.slider-inner').find('.z-1');
+	$('.slider-inner').find('.z-1', function() {
+		setTimeout(function(){$(this).removeClass("z-1");}, 400);
+	});
 	console.log(checker);
 	$(right).animate({left:940},400);
 	$(under).animate({left:0},400);
 	$(left).animate({left:-940},400);
-	setTimeout(function () { $('.slider-item').removeClass("z-1");},400);
 	$(main).animate({left:0},400).addClass("z-1");
 }
