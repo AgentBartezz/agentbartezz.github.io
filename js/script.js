bfc7d12cdefd7771ad45a6cae339b26aca7d0b8f
Bartezz <bhelak1991@gmail.com>
2016-05-21 19:27:46 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,6 +3,14 @@ $(window).load(function() {
 	
 	$(".jumper").click(function(){
 		var slide = $(this).attr("data-slider")	;
-		console.log(slide);
+	
+		if(slide == 1) {
+			slider(1,2,3,4);
+		}
 	});
 });
+
+function slide(a,b,c,d) {
+	var trigger = "#slider-image-" + a;
+	console.log(trigger);
+}
