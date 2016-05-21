f26b4004b03f9d6e40b2888dab606b1175890152
Bartezz <bhelak1991@gmail.com>
2016-05-21 19:40:08 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,9 +18,10 @@ function slider(a,b,c,d) {
 	var left = "#slider-item-" + d;
 	var right = "#slider-item-" + b;
 	var under = "#slider-item-" + c;
+	$('.slider-item').removeClass("z-1");
 	console.log(main);
-	$(main).animate({left:0},100);
-	$(right).animate({left:940},100);
-	$(under).animate({left:0},100);
-	$(left).animate({left:-940},0);
+	$(main).animate({left:0},400).addClass("z-1");
+	$(right).animate({left:940},400);
+	$(under).animate({left:0},400);
+	$(left).animate({left:-940},400);
 }
